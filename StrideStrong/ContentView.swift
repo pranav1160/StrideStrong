d2a4d824beb8558530cb2093dd85d4d89d8a65bc
Pranav <pranav1160ly@gmail.com>
2025-01-27 23:48:27 +0530

    first one!!!!!

--- StrideStrong/ContentView.swift
+++ StrideStrong/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Swiftui course babay!!")
+            Text("Swiftui course babay!!!")
         }
         .padding()
     }
