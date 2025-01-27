89bcd942d081d9adde3550affb6f53b818b3e272
Pranav <pranav1160ly@gmail.com>
2025-01-27 23:33:01 +0530

    Initial Commit

---
+++ StrideStrong/StrideStrongApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideStrongApp.swift
+//  StrideStrong
+//
+//  Created by Pranav on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct StrideStrongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
