f855e264edc4d45337208d144e91b61694f763a7
Pranav <pranav1160ly@gmail.com>
2025-01-28 09:37:56 +0530

    home screen ui activity card

---
+++ StrideStrong/home/ActivityCardView.swift
@@ -0,0 +1,54 @@
+//
+//  ActivityCardView.swift
+//  StrideStrong
+//
+//  Created by Pranav on 28/01/25.
+//
+
+import SwiftUI
+
+struct Activity{
+    let id:Int
+    let title:String
+    let subtitle:String
+    let image:String
+    let tintColor:Color
+    let amount:Int
+}
+
+struct ActivityCardView: View {
+    let myActivity:Activity
+    var body: some View {
+        ZStack{
+            Color(uiColor: .systemGray6)
+                .clipShape(.rect(cornerRadius: 15))
+            VStack{
+                HStack(alignment: .top){
+                    VStack(alignment: .leading){
+                        Text(myActivity.title)
+                        Text(myActivity.subtitle)
+                            .font(.caption)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: myActivity.image)
+                        .foregroundStyle(myActivity.tintColor)
+                    
+                }
+                
+                Text("\(myActivity.amount)")
+                    .font(.title)
+                    .bold()
+                    .padding()
+            }
+            .padding()
+            
+        }
+       
+    }
+}
+
+#Preview {
+    ActivityCardView(myActivity: Activity(id: 0, title: "Todays Steps", subtitle: "Goal 10,000", image: "figure.walk", tintColor: .green, amount: 9172))
+}
--- StrideStrong/home/HomeView.swift
+++ StrideStrong/home/HomeView.swift
@@ -11,6 +11,16 @@ struct HomeView: View {
     @State var currCalories = 123
     @State var currActiveHrs = 52
     @State var currStand = 8
+    
+//    let myColumns=[GridItem(spacing: 20),GridItem(spacing: 20)]
+    
+    let mockActivity = [
+        Activity(id: 0, title: "Today's Steps", subtitle: "Goal 10,000", image: "figure.walk", tintColor: .green, amount: 9172),
+        Activity(id: 1, title: "Calories Burned", subtitle: "Goal 2000", image: "figure.walk", tintColor: .red, amount: 1850),
+        Activity(id: 2, title: "Distance Run", subtitle: "Goal 5km", image: "figure.run", tintColor: .blue, amount: 4200),
+        Activity(id: 3, title: "Hours Slept", subtitle: "Goal 8h", image: "figure.walk", tintColor: .purple, amount: 6)
+    ]
+    
     var body: some View {
         ScrollView {
             //welcme text
@@ -65,6 +75,31 @@ struct HomeView: View {
                 Spacer()
             }
             .padding()
+            
+            HStack{
+                Text("Fitness Activity")
+                    .font(.title2)
+                
+                Spacer()
+                
+                Button{
+                    print("printing show more")
+                }label: {
+                    Text("Show More")
+                        .padding(.all,10)
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .clipShape(.rect(cornerRadius: 20))
+                }
+            }.padding(.horizontal)
+            
+            LazyVGrid(columns: Array(repeating: GridItem(spacing: 20), count: 2)){
+                ForEach(mockActivity,id: \.id) { activity in
+                    ActivityCardView(myActivity: activity)
+                }
+            }
+            .padding(.horizontal)
+            
         }
         .preferredColorScheme(.dark)
     }
